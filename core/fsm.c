75d505e9073df2c6424b594d5d77531c4ffe0a64
gaccob <weiye.zhang@qq.com>
2014-01-08 01:00:19 +0800

    add fsm, not completed

--- core/fsm.c
+++ core/fsm.c
@@ -1,5 +1,10 @@
 #include "fsm.h"
 
+#define FSM_STATUS_SIZE sizeof(fsm_status_t)
+#define FSM_RULES_SIZE(nstatus) ((nstatus * (nstatus - 1)) >> 1)
+#define FSM_EVENT_SIZE(nstatus) (sizeof(fsm_event_t) \
+    + (sizeof(fsm_rule_t*) * FSM_RULES_SIZE(nstatus))
+
 fsm_t* fsm_init(int status_size, int events_size)
 {
     fsm_t* fsm;
@@ -12,16 +17,17 @@ fsm_t* fsm_init(int status_size, int events_size)
 
     fsm->status_size = status_size;
     fsm->status_count = 0;
-    fsm->status = (fsm_status_t*)MALLOC(sizeof(fsm_status_t) * fsm->status_size);
+    fsm->status = (fsm_status_t*)MALLOC(FSM_STATUS_SIZE * fsm->status_size);
     fsm->status_table = idtable_init(fsm->status_size);
     assert(fsm->status && fsm->status_table);
 
     fsm->events_size = events_size;
     fsm->events_count = 0;
-    fsm->events = (fsm_event_t*)MALLOC(sizeof(fsm_event_t) * fsm->events_size);
+    fsm->events = (fsm_event_t*)MALLOC(FSM_EVENT_SIZE(status_size) * fsm->events_size);
     fsm->events_table = idtable_init(fsm->events_size);
     assert(fsm->events && fsm->events_table);
 
+    fsm->current = FSM_INVALID_STATUS;
     return fsm;
 }
 
@@ -31,7 +37,7 @@ int fsm_register_status(fsm_t* fsm, int status, const char* name,
     fsm_status_t* st;
     int ret;
 
-    if (!fsm || !name) {
+    if (!fsm || !name || status <= 0) {
         return FSM_FAIL;
     }
     if (fsm->status_count >= fsm->status_size) {
@@ -56,27 +62,34 @@ int fsm_register_event(fsm_t* fsm, int event, const char* name,
                        int from_status, int to_status, fsm_event_func_t handle)
 {
     fsm_event_t* et;
+    fsm_rule_t* rule;
     int ret;
 
-    if (!fsm || !name) {
+    if (!fsm || !name || event <= 0) {
         return FSM_FAIL;
     }
     if (fsm->events_count >= fsm->events_size) {
         return FSM_FULL;
     }
-    if (idtabel_get(fsm->events_table, event)) {
-        return FSM_EXISTED;
+
+    et = (fsm_event_t*)idtabel_get(fsm->events_table, event);
+    if (!et) { 
+        et = &fsm->events[fsm->events_count ++];
+        et->id = event;
+        et->rules_count = 0;
+        snprintf(et->name, sizeof(et->name), "%s", name);
+        ret = idtable_add(st->events_table, et->id, (void*)et);
+        assert(ret == 0);
     }
 
-    et = &fsm->events[fsm->events_count ++];
-    et->id = event;
-    snprintf(et->name, sizeof(et->name), "%s", name);
-    et->from_status = from_status;
-    et->to_status = to_status;
-    et->handle = handle;
-    ret = idtable_add(st->events_table, et->id, (void*)et);
-    assert(ret == 0);
-
+    if (et->rules_count >= FSM_RULES_SIZE(fsm->status_size)) {
+        return FSM_FULL;
+    }
+    et->rules[et->rules_count] = (fsm_rule_t*)MALLOC(sizeof(fsm_rule_t));
+    rule = et->rules[et->rules_count ++];
+    rule->from_status = from_status;
+    rule->to_status = to_status;
+    rule->handle = handle;
     return FSM_OK;
 }
 
--- core/fsm.h
+++ core/fsm.h
@@ -6,36 +6,35 @@ extern "C" {
 #endif
 
 #define FSM_NAME_LEN 32
-
 #define FSM_INVALID_STATUS -1
 
 typedef void (*fsm_status_func_t) (void* args);
-
 typedef struct fsm_status_t {
     int id;
-
-    // name for debug
     char name[FSM_NAME_LEN];
-
     // enter & exit will not happend when status enter itself
     fsm_status_func_t enter;
     fsm_status_func_t exit;
 } fsm_status_t;
 
 typedef int (*fsm_event_func_t) (void* args);
-
-typedef struct fsm_event_t {
-    int id;
-
-    // name for debug
-    char name[FSM_NAME_LEN];
-
+typedef struct fsm_rule_t {
     int from_status;
     int to_status;
     fsm_event_func_t handle;
+} fsm_rule_t;
+
+typedef struct fsm_event_t {
+    int id;
+    char name[FSM_NAME_LEN];
+    // rules size: (status size) ^ 2 / 2
+    int rules_count;
+    fsm_rule_t* rules[0];
 } fsm_event_t;
 
 typedef struct fsm_t {
+    int current;
+
     int status_size;
     int status_count;
     fsm_status_t* status;
@@ -65,16 +64,24 @@ int fsm_register_event(struct fsm_t* fsm, int event, const char* name,
 
 int fsm_trigger(struct fsm_t* fsm, int event, void* args)
 {
+    int i, ret;
+    fsm_rule_t* rule;
+
     if (!fsm) {
         return FSM_FAIL;
     }
-
     fsm_event_t* et = (fsm_event_t*)idtable_get(fsm->events_table, event);
     if (!et) {
         return FSM_NOT_EXISTED;
     }
 
-
+    for (i = 0; i < et->rules_count; ++ i) {
+        rule = et->rules[i];
+        if (rule->from_status == fsm->current) {
+            ret = rule->handle(args);
+            break;
+        }
+    }
 }
 
 #ifdef __cplusplus
